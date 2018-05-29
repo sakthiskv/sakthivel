6eac5e9f31b61b09824820fba4851e4de6523f2b
sakthiskv <romansakthi18@gmail.com>
2018-05-29 00:02:55 -0700

    complement example

---
+++ complement.java
@@ -0,0 +1,22 @@
+public class Complement{
+ 
+    public static void main (String[] args) 
+{
+  
+      int count=10;
+ 
+  System.out.println(~1);       
+      count++;
+       
+     count--;
+     
+   System.out.println(count);
+ 
+boolean isTrue=false;
+System.out.println(!isTrue);
+   System.out.println(~count);
+ 
+   
+}
+
+}
