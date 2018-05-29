20c1b35bbd0b2f68d30c62c9a2c02d46a09774a3
sakthiskv <romansakthi18@gmail.com>
2018-05-28 23:35:55 -0700

    hi

---
+++ OperatorExample.java
@@ -0,0 +1,16 @@
+public class OperatorExample{
+ 
+    public static void main (String[] args) 
+{
+  
+      int count=10;
+        
+      count++;
+       
+     count--;
+     
+   System.out.println(count);
+    
+}
+
+}
